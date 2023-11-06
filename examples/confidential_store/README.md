fb88a032b7ed70ac3e33187a9293142e91ac7e25
Ferran Borreguero <ferranbt@protonmail.com>
2023-11-06 12:00:58 +0100

    First batch of examples

---
+++ examples/confidential_store/README.md
@@ -0,0 +1,3 @@
+# Example Suapp with Confidential Store
+
+This example features how to use the Confidential Store to store and retrieve a secret.
---
+++ examples/confidential_store/confidential_store.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.8;
+
+import "../../suave-geth/suave/sol/libraries/Suave.sol";
+
+contract ConfidentialStore {
+    address[] public addressList = [0xC8df3686b4Afb2BB53e60EAe97EF043FE03Fb829];
+
+    function example() external payable {
+        Suave.Bid memory bid = Suave.newBid(
+            10,
+            addressList,
+            addressList,
+            "namespace"
+        );
+
+        Suave.confidentialStore(bid.id, "key1", abi.encode(1));
+        Suave.confidentialStore(bid.id, "key2", abi.encode(2));
+    }
+}
