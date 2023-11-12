c70e1a25d6fb84d2f5095b0ff70803dba5c3711a
Ferran Borreguero <ferran.borreguero@gmail.com>
2023-11-13 00:42:31 +0300

    More stuff

--- examples/app-rfq/rfq.sol
+++ examples/app-rfq/rfq.sol
@@ -4,64 +4,64 @@ pragma solidity ^0.8.8;
 import "../../suave-geth/suave/sol/libraries/Suave.sol";
 
 contract RfqDriver {
-    address[] solvers;
-    Context currentContext;
-
-    struct Context {
-        string namespace;
-        uint64 cond;
-        address[] solvers;
+    constructor(address[] memory solvers) {
+        // create a threshold signature
     }
 
-    struct Proposal {
-        uint64 priority;
-        bytes body;
+    event EmitOrder(bytes order);
+
+    function submitOrderCallback(bytes memory order) external payable {
+        emit EmitOrder(order);
+    }
+    
+    function submitOrder(bytes memory order) external payable returns (bytes memory) {
+        bytes memory orderEncrypted = encryptOrder(order);
+        return abi.encodeWithSelector(this.submitOrderCallback.selector, orderEncrypted);
+    }
+    
+    function encryptOrder(bytes memory order) internal returns (bytes memory) {
+        bytes memory orderEncrypted;
+        return orderEncrypted;
     }
 
-    function submitOrder(bytes memory order) external payable {
-        Suave.Bid memory bid = Suave.newBid(
-            currentContext.cond, 
-            currentContext.solvers, 
-            currentContext.solvers, 
-            string.concat(currentContext.namespace, "/orders")
-        );
-        Suave.confidentialStore(bid.id, "data", order);
+    function submitSolution(bytes memory solution) external payable {
+        uint64 score = evaluateSolution(solution);
+
+        // Store the solution in the confidential store
+        address[] emptyAddr;
+
+        Suave.Bid memory bid = Suave.newBid(0, emptyAddr, emptyAddr, "namespace");
+        Suave.confidentialStore(bid.id, "solution", solution);
+        Suave.confidentialStore(bid.id, "score", abi.encode(score));
     }
 
-    function generateOrders(Context memory newContext) external payable returns (bytes memory) {
-        // aggregate all the proposals.
-        // TODO: Aggregator logic.
-        
-        // update the current bid on chain
-        return abi.encodeWithSelector(this.updateContext.selector, newContext);
+    function evaluateSolution(bytes memory solution) internal pure returns (uint64) {
+        // TODO: Score based on user execution
+        return 0;
     }
 
-    function updateContext(Context memory newContext) external payable {
-        currentContext = newContext;
+    function settle() public payable {
+        Suave.Bid[] memory solutions = Suave.fetchBids(10, "namespace");
+        require(solutions.length > 0, "No bids available");
+
+        uint64 highestScore = 0;
+        uint256 winningBidIndex;
+
+        for (uint256 i = 0; i < solutions.length; i++) {
+            uint64 score = abi.decode(Suave.confidentialRetrieve(solutions[i].id, "score"), (uint64));
+
+            if (score > highestScore) {
+                highestScore = score;
+                winningBidIndex = i;
+            }
+        }
+
+        Suave.Bid memory winningBid = solutions[winningBidIndex];
+        bytes memory solution = Suave.confidentialRetrieve(winningBid.id, "solution");
+
+        emitWinningSolutionToL1(solution);
     }
-}
 
-contract RfqSolver {
-    address[] public addressList;
-
-    constructor(address _driver) {
-        addressList.push(_driver);
-    }
-
-    // solve fetches the current user orders and solves
-    function solve(RfqDriver.Context memory context) external payable {
-        Suave.Bid[] memory allBids = Suave.fetchBids(context.cond, context.namespace);
-
-        // TODO: SOLVER LOGIC
-
-        // write the proposal
-        RfqDriver.Proposal memory p;
-
-        Suave.Bid memory bid = Suave.newBid(
-            context.cond, 
-            addressList, 
-            addressList, 
-            string.concat(context.namespace, "/solution")
-        );
+    function emitWinningSolutionToL1(bytes memory solution) internal {
     }
 }
