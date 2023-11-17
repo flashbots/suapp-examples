ed8e922f15011c413235e04dddb800044a53744f
Ferran Borreguero <ferran.borreguero@gmail.com>
2023-11-17 10:23:15 +0300

    Fix examples

--- examples/mevm-confidential-store/confidential-store.sol
+++ examples/mevm-confidential-store/confidential-store.sol
@@ -4,15 +4,13 @@ pragma solidity ^0.8.8;
 import "../../suave-geth/suave/sol/libraries/Suave.sol";
 
 contract ConfidentialStore {
-    address[] public addressList = [0xC8df3686b4Afb2BB53e60EAe97EF043FE03Fb829];
-
     function callback() external payable {}
 
-    function example() external payable returns (bytes memory) {
+    function example(address[] memory allowedList) external payable returns (bytes memory) {
         Suave.Bid memory bid = Suave.newBid(
             10,
-            addressList,
-            addressList,
+            allowedList,
+            allowedList,
             "namespace"
         );
 
@@ -23,8 +21,6 @@ contract ConfidentialStore {
         require(keccak256(value) == keccak256(abi.encode(1)));
 
         Suave.Bid[] memory allShareMatchBids = Suave.fetchBids(10, "namespace");
-        require(allShareMatchBids.length > 1);
-
         return abi.encodeWithSelector(this.callback.selector);
     }
 }
--- examples/mevm-confidential-store/main.go
+++ examples/mevm-confidential-store/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/flashbots/suapp-examples/framework"
 )
 
 func main() {
 	fr := framework.New()
-	fr.DeployContract("confidential-store.sol/ConfidentialStore.json").
-		SendTransaction("example", nil, nil)
+	contract := fr.DeployContract("confidential-store.sol/ConfidentialStore.json")
+	contract.SendTransaction("example", []interface{}{[]common.Address{contract.Address()}}, nil)
 }
