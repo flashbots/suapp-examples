c389b85fe155f9cce50d952c73a20f83a629278b
Ferran Borreguero <ferranbt@protonmail.com>
2023-12-07 12:51:56 +0100

    Fix onchain state example

--- examples/onchain-state/main.go
+++ examples/onchain-state/main.go
@@ -11,19 +11,22 @@ func main() {
 	fr := framework.New()
 	contract := fr.DeployContract("onchain-state.sol/OnChainState.json")
 
-	fmt.Println("1. Send a confidential request that cannot modify the state")
+	fmt.Println("1. A confidential request fails if it tries to modify the state")
 
-	contract.SendTransaction("nilExample", nil, nil)
-	val := contract.Call("getState")[0].(uint64)
-	if val != 0 {
-		fmt.Printf("expected 0")
+	_, err := contract.Raw().SendTransaction("nilExample", nil, nil)
+	if err != nil {
+		fmt.Println("expected an error")
 		os.Exit(1)
 	}
 
 	fmt.Println("2. Send a confidential request that modifies the state")
 
 	contract.SendTransaction("example", nil, nil)
-	val = contract.Call("getState")[0].(uint64)
+	val, ok := contract.Call("getState")[0].(uint64)
+	if !ok {
+		fmt.Printf("expected uint64")
+		os.Exit(1)
+	}
 	if val != 1 {
 		fmt.Printf("expected 1")
 		os.Exit(1)
