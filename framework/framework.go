ed15cef0a9e2dc8538a7e232771c6aaafbf2939f
andytudhope <13001517+andytudhope@users.noreply.github.com>
2024-01-09 15:27:54 +0200

    spelling fix

--- framework/framework.go
+++ framework/framework.go
@@ -173,7 +173,7 @@ func DefaultConfig() *Config {
 	return &Config{
 		KettleRPC: "http://localhost:8545",
 
-		// This account is funded in both devnev networks
+		// This account is funded in your local SUAVE devnet
 		// address: 0xBE69d72ca5f88aCba033a063dF5DBe43a4148De0
 		FundedAccount: NewPrivKeyFromHex("91ab9a7e53c220e6210460b65a7a3bb2ca181412a8a7b43ff336b3df1737ce12"),
 	}
