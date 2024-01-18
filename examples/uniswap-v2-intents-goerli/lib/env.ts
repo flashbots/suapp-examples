c066fe3b935b60472ac2921e283e4427206b0f8b
Brock Smedley <2791467+zeroXbrock@users.noreply.github.com>
2024-01-18 15:18:28 -0800

    move .env[.example] up from specific example to project root

--- examples/uniswap-v2-intents-goerli/lib/env.ts
+++ examples/uniswap-v2-intents-goerli/lib/env.ts
@@ -1,15 +1,19 @@
 import dotenv from "dotenv"
-dotenv.config()
+import path from "path"
+
+dotenv.config({
+    path: path.resolve(import.meta.dir, "../../../.env")
+})
 
 function loadEnv() {
     let GOERLI_KEY = process.env.GOERLI_KEY
     let SUAVE_KEY = process.env.SUAVE_KEY
     // prepend 0x if var exists and 0x is not present
     if (GOERLI_KEY && !GOERLI_KEY.startsWith("0x")) {
-        GOERLI_KEY = "0x" + GOERLI_KEY
+        GOERLI_KEY = `0x${GOERLI_KEY}`
     }
     if (SUAVE_KEY && !SUAVE_KEY.startsWith("0x")) {
-        SUAVE_KEY = "0x" + SUAVE_KEY
+        SUAVE_KEY = `0x${SUAVE_KEY}`
     }
     return {
         SUAVE_KEY,
