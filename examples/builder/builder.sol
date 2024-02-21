bb7062b3a926af877ca9ef0bf97a367c19634827
Louis Thibault <l.thibault@sentimens.com>
2024-02-21 14:44:36 -0500

    Deploy contract and call example method stub.

---
+++ examples/builder/builder.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.8;
+
+import "suave-std/suavelib/Suave.sol";
+
+contract Builder {
+    function callback() external payable {}
+
+    function example() external payable returns (bytes memory) {
+        // address[] memory allowedList = new address[](1);
+        // allowedList[0] = address(this);
+
+        // Suave.DataRecord memory dataRecord = Suave.newDataRecord(10, allowedList, allowedList, "namespace");
+
+        // Suave.confidentialStore(dataRecord.id, "key1", abi.encode(1));
+        // Suave.confidentialStore(dataRecord.id, "key2", abi.encode(2));
+
+        // bytes memory value = Suave.confidentialRetrieve(dataRecord.id, "key1");
+        // require(keccak256(value) == keccak256(abi.encode(1)));
+
+        // Suave.DataRecord[] memory allShareMatchBids = Suave.fetchDataRecords(10, "namespace");
+        return abi.encodeWithSelector(this.callback.selector);
+    }
+}
--- examples/builder/main.go
+++ examples/builder/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "os"
+import (
+	"github.com/flashbots/suapp-examples/framework"
+)
 
 func main() {
-	os.Exit(99)
+	fr := framework.New()
+	r := fr.Suave.
+		DeployContract("builder.sol/Builder.json").
+		SendTransaction("example", []interface{}{}, nil)
+	if r == nil {
+		panic(r)
+	}
 }
