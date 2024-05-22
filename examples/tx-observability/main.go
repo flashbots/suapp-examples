10ecca85780f8a6444c1c690c373bb1c7456adaa
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2024-05-22 16:13:34 -0700

    add tx observability example

---
+++ examples/tx-observability/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/flashbots/suapp-examples/framework"
+)
+
+// SentTransactionsEvent is emitted by SUAPPs to indicate that the SUAPP sent some transactions to L1.
+type SentTransactionsEvent struct {
+	TxHashes []common.Hash `abi:"txHashes"`
+}
+
+func main() {
+	fr := framework.New(framework.WithL1())
+	suappContract := fr.Suave.DeployContract("observability.sol/Suapp.json")
+
+	res := suappContract.SendConfidentialRequest("doSomethingWithTxs", nil, nil)
+	if res.Status != 1 {
+		log.Fatal("confidential request failed")
+	}
+
+	logEntry := types.Log{
+		Data:   res.Logs[0].Data,
+		Topics: res.Logs[0].Topics,
+	}
+	var event SentTransactionsEvent
+	err := suappContract.Abi.UnpackIntoInterface(&event, "SentTransactions", logEntry.Data)
+	if err != nil {
+		log.Fatalf("Failed to unpack log data: %v", err)
+	}
+	log.Printf("logged tx hashes: %v", event.TxHashes)
+}
