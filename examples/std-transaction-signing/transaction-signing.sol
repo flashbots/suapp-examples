2c1030c74ca88e82670776d90cc44f1883705450
Marcos <marcoscarlomagno1@gmail.com>
2024-06-18 07:49:36 -0300

    format

--- examples/std-transaction-signing/transaction-signing.sol
+++ examples/std-transaction-signing/transaction-signing.sol
@@ -16,21 +16,17 @@ contract TransactionSigning is Suapp {
     function example() public returns (bytes memory) {
         string memory signingKey = string(Context.confidentialInputs());
 
-        Transactions.EIP155Request memory txnWithToAddress = Transactions
-            .EIP155Request({
-                to: address(0x00000000000000000000000000000000DeaDBeef),
-                gas: 1000000,
-                gasPrice: 500,
-                value: 1,
-                nonce: 1,
-                data: bytes(""),
-                chainId: 1337
-            });
+        Transactions.EIP155Request memory txnWithToAddress = Transactions.EIP155Request({
+            to: address(0x00000000000000000000000000000000DeaDBeef),
+            gas: 1000000,
+            gasPrice: 500,
+            value: 1,
+            nonce: 1,
+            data: bytes(""),
+            chainId: 1337
+        });
 
-        Transactions.EIP155 memory txn = Transactions.signTxn(
-            txnWithToAddress,
-            string(signingKey)
-        );
+        Transactions.EIP155 memory txn = Transactions.signTxn(txnWithToAddress, string(signingKey));
 
         emit TxnSignature(txn.r, txn.s);
 
