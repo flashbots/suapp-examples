896ca674297960f3e6ba63b81a35569ba9354424
Ferran Borreguero <ferran.borreguero@gmail.com>
2024-10-31 12:22:41 +0000

    Query multiple bids (#69)
    * Query multiple bids
    
    * Lock Go version in CI
    
    * Follow lint
    
    * Fix other CI

--- examples/mevm-confidential-store/main.go
+++ examples/mevm-confidential-store/main.go
@@ -6,6 +6,11 @@ import (
 
 func main() {
 	fr := framework.New()
-	fr.Suave.DeployContract("confidential-store.sol/ConfidentialStore.json").
-		SendConfidentialRequest("example", []interface{}{}, nil)
+
+	namespace := framework.RandomString(10)
+	ctr := fr.Suave.DeployContract("confidential-store.sol/ConfidentialStore.json")
+
+	ctr.SendConfidentialRequest("example", []interface{}{namespace}, nil)
+	ctr.SendConfidentialRequest("example2", []interface{}{namespace}, nil)
+	ctr.SendConfidentialRequest("query", []interface{}{namespace}, nil)
 }
